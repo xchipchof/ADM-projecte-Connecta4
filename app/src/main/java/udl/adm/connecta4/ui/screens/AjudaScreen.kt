0dfa634148df1f150f481210cafe2745186584ae
xchipchof <diegortandorra@gmail.com>
2026-04-14 18:55:45 +0200

    ui screens implementation
    Pantalles implementades pel moment:
    - pantalla d'ajuda
    - Home screen (Menu principal de la app)

---
+++ app/src/main/java/udl/adm/connecta4/ui/screens/AjudaScreen.kt
@@ -0,0 +1,35 @@
+package udl.adm.connecta4.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AjudaScreen(onBack: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("AJUDA", style = MaterialTheme.typography.headlineMedium)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            "El joc consisteix en anar introduint fitxes en un tauler vertical...\n" +
+                    "Guanya qui alinea 4 consecutives (H, V o D).\n" +
+                    "Tu ets el VERMELL, la màquina el GROC.",
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(onClick = onBack) { Text("Anar al joc") }
+    }
+}
---
+++ app/src/main/java/udl/adm/connecta4/ui/screens/MenuPrincipalScreen.kt
@@ -0,0 +1,32 @@
+package udl.adm.connecta4.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MenuPrincipalScreen(onAjudaClick: () -> Unit, onComencarClick: () -> Unit, onExitClick: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "CONNECT 4", style = MaterialTheme.typography.headlineLarge)
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(onClick = onAjudaClick) { Text("Ajuda") }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onComencarClick) { Text("Començar Partida") }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onExitClick) { Text("Sortir") }
+    }
+}
