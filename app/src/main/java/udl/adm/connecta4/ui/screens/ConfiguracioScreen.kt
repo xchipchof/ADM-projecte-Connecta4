fd176bd2ded9d9242979700caead75a27bbead57
xchipchof <diegortandorra@gmail.com>
2026-04-16 10:27:05 +0200

    Config Screen implemented
    [v1] Implemented the Configuració Screen
    [v2] Refactored the main composable into multiple stateless composables,
    and one statefull composable (for the time being)

---
+++ app/src/main/java/udl/adm/connecta4/ui/screens/ConfiguracioScreen.kt
@@ -0,0 +1,128 @@
+package udl.adm.connecta4.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import udl.adm.connecta4.ui.Connecta4Theme
+
+@Composable
+fun ConfiguracioScreen(onStartGame: (String, Int, Boolean, Int) -> Unit) {
+    var alias by remember { mutableStateOf("p1") }
+    var size by remember { mutableStateOf(7) }
+    var hasTime by remember { mutableStateOf(false) }
+    var maxTime by remember { mutableStateOf("25") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text("CONFIGURACIÓ", style = MaterialTheme.typography.headlineMedium)
+
+        AliasTextField(alias = alias) { alias = it }
+
+        GridSizeSelectionBlock(size = size) { opt -> size = opt }
+
+
+        TimeControlOption(hasTime) { hasTime = it }
+
+        TimeControlTextField(hasTime, maxTime) { maxTime = it }
+
+        Spacer(Modifier.weight(1f))
+        Button(
+            onClick = {
+                val time = maxTime.toIntOrNull() ?: 25
+                onStartGame(alias.ifBlank { "p1" }, size, hasTime, time)
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Començar")
+        }
+    }
+}
+
+@Composable
+fun AliasTextField(alias: String, onValueChange: (String) -> Unit) {
+    Spacer(Modifier.height(16.dp))
+
+    OutlinedTextField(
+        value = alias, onValueChange = onValueChange,
+        label = { Text("Alias identificatiu") },
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun GridSizeSelectionBlock(size: Int, onValueChange: (Int) -> Unit) {
+    Spacer(Modifier.height(16.dp))
+    Column() {
+        Text("Mida graella", modifier = Modifier.padding(10.dp))
+        Row {
+            listOf(7, 6, 5).forEach { opt ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(selected = size == opt, onClick = { onValueChange(opt) })
+                    Text(opt.toString())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TimeControlOption(hasTime: Boolean, onCheckedChange: (Boolean) -> Unit) {
+    Spacer(Modifier.height(16.dp))
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Checkbox(checked = hasTime, onCheckedChange = onCheckedChange)
+        Text("Control del temps")
+    }
+}
+
+@Composable
+fun TimeControlTextField(hasTime: Boolean, maxTime: String, onValueChange: (String) -> Unit) {
+    if (hasTime) {
+        OutlinedTextField(
+            value = maxTime, onValueChange = onValueChange,
+            label = { Text("Segons") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview(showBackground = true, heightDp = 300)
+@Composable
+fun GridSizeSelectionBlockPreview() {
+    Connecta4Theme {
+        GridSizeSelectionBlock(7) {}
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ConfiguracioScreenPreview() {
+    Connecta4Theme {
+        ConfiguracioScreen(onStartGame = { _, _, _, _ -> })
+    }
+}
