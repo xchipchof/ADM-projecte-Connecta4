0750cb35d9cc142a6bc9cbd7dcdf3f7e7ea8d858
xchipchof <diegortandorra@gmail.com>
2026-04-23 22:20:48 +0200

    UI refactor: applied the viewModel usage in the Screen composable
    Also modularized the composable into different components, so they can
    be further customized.

--- app/src/main/java/udl/adm/connecta4/ui/screens/ResultatsScreen.kt
+++ app/src/main/java/udl/adm/connecta4/ui/screens/ResultatsScreen.kt
@@ -1,10 +1,11 @@
 package udl.adm.connecta4.ui.screens
 
+import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.util.Patterns
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,57 +17,124 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import androidx.core.net.toUri
+import udl.adm.connecta4.viewmodel.ResultatsViewModel
 
 @Composable
-fun ResultatsScreen(log: String, onNewGame: () -> Unit, onExit: () -> Unit) {
-    val context = LocalContext.current
-    var email by remember { mutableStateOf("professor@domain.cat") }
-    val dateTime = remember { SimpleDateFormat("d/M/yy, HH:mm", Locale.getDefault()).format(Date()) }
+fun ResultatsScreen(
+    resViewModel: ResultatsViewModel,
+    log: String,
+    onNewGame: () -> Unit,
+    onExit: () -> Unit,
+) {
 
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-        Text("RESULTATS PARTIDA", style = MaterialTheme.typography.headlineMedium)
-        Spacer(Modifier.height(16.dp))
 
-        OutlinedTextField(value = dateTime, onValueChange = {}, label = { Text("Dia i hora") }, readOnly = true, modifier = Modifier.fillMaxWidth())
-        Spacer(Modifier.height(8.dp))
-
-        OutlinedTextField(value = log, onValueChange = {}, label = { Text("Valors del Log") }, readOnly = true, modifier = Modifier.fillMaxWidth().height(150.dp))
-        Spacer(Modifier.height(8.dp))
-
-        OutlinedTextField(value = email, onValueChange = { email = it }, label = { Text("E-mail destinatari") }, modifier = Modifier.fillMaxWidth())
-
-        Spacer(Modifier.weight(1f))
-
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = {
-                if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    val intent = Intent(Intent.ACTION_SENDTO).apply {
-                        data = Uri.parse("mailto:")
-                        putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
-                        putExtra(Intent.EXTRA_SUBJECT, "Log - $dateTime")
-                        putExtra(Intent.EXTRA_TEXT, log)
-                    }
-                    context.startActivity(Intent.createChooser(intent, "Send Email"))
-                } else {
-                    Toast.makeText(context, "Correu invàlid", Toast.LENGTH_SHORT).show()
-                }
+    ResultatsContent(
+        log = log, email = resViewModel.email, dateTime = resViewModel.dateTime,
+        emailChanger = { resViewModel.onEmailChange(it) }, onNewGame = onNewGame, onExit = onExit
+    )
+    { email: String, dateTime: String, context: Context ->
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = "mailto:".toUri()
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+                putExtra(Intent.EXTRA_SUBJECT, "Log - $dateTime")
+                putExtra(Intent.EXTRA_TEXT, log)
             }
-        ) { Text("Enviar e-mail") }
-
-        Spacer(Modifier.height(8.dp))
-        Button(onClick = onNewGame, modifier = Modifier.fillMaxWidth()) { Text("Nova partida") }
-        Spacer(Modifier.height(8.dp))
-        Button(onClick = onExit, modifier = Modifier.fillMaxWidth(), colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error)) { Text("Sortir") }
+            context.startActivity(Intent.createChooser(intent, "Send Email"))
+        } else {
+            Toast.makeText(context, "Correu invàlid", Toast.LENGTH_SHORT).show()
+        }
     }
+}
+
+@Composable
+fun ResultatsContent(
+    log: String,
+    email: String,
+    dateTime: String,
+    emailChanger: (String) -> Unit,
+    onNewGame: () -> Unit,
+    onExit: () -> Unit,
+    emailSender: (String, String, Context) -> Unit
+) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        ResultsData(dateTime, log)
+        OutlinedTextField(
+            value = email,
+            onValueChange = emailChanger,
+            label = { Text("E-mail destinatari") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        SendEmailButton(emailSender, email, dateTime, context)
+
+        NewGameButton(onNewGame)
+        ExitButton(onExit)
+    }
+}
+
+@Composable
+private fun ColumnScope.SendEmailButton(
+    emailSender: (String, String, Context) -> Unit,
+    email: String,
+    dateTime: String,
+    context: Context
+) {
+    Spacer(Modifier.weight(1f))
+
+    Button(
+        modifier = Modifier.fillMaxWidth(),
+        onClick = { emailSender(email, dateTime, context) }
+    ) { Text("Enviar e-mail") }
+}
+
+@Composable
+private fun NewGameButton(onNewGame: () -> Unit) {
+    Spacer(Modifier.height(8.dp))
+    Button(onClick = onNewGame, modifier = Modifier.fillMaxWidth()) { Text("Nova partida") }
+}
+
+@Composable
+private fun ExitButton(onExit: () -> Unit) {
+    Spacer(Modifier.height(8.dp))
+    Button(
+        onClick = onExit,
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error)
+    ) { Text("Sortir") }
+}
+
+@Composable
+private fun ResultsData(dateTime: String, log: String) {
+    Text("RESULTATS PARTIDA", style = MaterialTheme.typography.headlineMedium)
+    Spacer(Modifier.height(16.dp))
+
+    OutlinedTextField(
+        value = dateTime,
+        onValueChange = {},
+        label = { Text("Dia i hora") },
+        readOnly = true,
+        modifier = Modifier.fillMaxWidth()
+    )
+    Spacer(Modifier.height(8.dp))
+
+    OutlinedTextField(
+        value = log,
+        onValueChange = {},
+        label = { Text("Valors del Log") },
+        readOnly = true,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+    )
+    Spacer(Modifier.height(8.dp))
 }
--- app/src/main/java/udl/adm/connecta4/viewmodel/ResultatsViewModel.kt
+++ app/src/main/java/udl/adm/connecta4/viewmodel/ResultatsViewModel.kt
@@ -4,6 +4,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class ResultatsViewModel(val initialLog: String) : ViewModel() {
     var logText by mutableStateOf(initialLog)
@@ -12,8 +16,21 @@ class ResultatsViewModel(val initialLog: String) : ViewModel() {
     var email by mutableStateOf("")
         private set
 
+    var dateTime by mutableStateOf(SimpleDateFormat("d/M/yy, HH:mm", Locale.getDefault()).format(Date()))
+        private set
+
     fun onEmailChange(newEmail: String) {
         email = newEmail
+    }
+
+    class Factory(private val initLog: String) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(ResultatsViewModel::class.java)){
+                @Suppress("UNCHECKED_CAST")
+                return ResultatsViewModel(initLog) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
     }
 
 }
