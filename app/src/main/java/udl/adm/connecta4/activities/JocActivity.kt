a0d60208ebf0ac2435653761a46534be9f5c0b9f
xchipchof <diegortandorra@gmail.com>
2026-05-23 22:31:01 +0200

    refactor: DataStore implemented into activity's core logic
    Ara la activitat llança una query ( amb lifeCyckeScope.launch ) per
    obtenir les preferències del usuari e inicialitzar la partida. Ara ja no
    fa falta inicialitzar el viewModel amb la factory perqué es un viewModel
    amb context de la aplicació, per lo que amb viewModels ja furula.

--- app/src/main/java/udl/adm/connecta4/activities/JocActivity.kt
+++ app/src/main/java/udl/adm/connecta4/activities/JocActivity.kt
@@ -5,39 +5,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import kotlin.properties.Delegates
 import udl.adm.connecta4.ui.Connecta4Theme
 import udl.adm.connecta4.ui.screens.JocScreen
 import udl.adm.connecta4.viewmodel.GameViewModel
-import kotlin.properties.Delegates
 
 class JocActivity : ComponentActivity() {
 
-    private lateinit var a :String
+    private lateinit var a: String
     private var s by Delegates.notNull<Int>()
     private var h by Delegates.notNull<Boolean>()
     private var m by Delegates.notNull<Int>()
 
-    private val jocViewModel : GameViewModel by viewModels {
-        GameViewModel.Factory(a,s,h,m)
+    private val jocViewModel: GameViewModel by viewModels {
+        GameViewModel.Factory(application, a, s, h, m)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        a = intent.getStringExtra("ALIAS") ?: "p1"
-        s = intent.getIntExtra("SIZE", 7)
+        a = intent.getStringExtra("ALIAS")   ?: "Jugador"
+        s = intent.getIntExtra("SIZE",     7)
         h = intent.getBooleanExtra("HAS_TIME", false)
         m = intent.getIntExtra("MAX_TIME", 25)
 
+        @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
         setContent {
-            Connecta4Theme() {
+            Connecta4Theme {
+                val windowSizeClass = calculateWindowSizeClass(this)
                 JocScreen(
-                    viewModel = jocViewModel,
-                    onGameEnd = { log ->
-                        val intent = Intent(this, ResultatsActivity::class.java).apply {
-                            putExtra("LOG_DATA", log)
-                        }
-                        startActivity(intent)
+                    viewModel       = jocViewModel,
+                    windowSizeClass = windowSizeClass,
+                    onGameEnd       = { log ->
+                        startActivity(
+                            Intent(this, ResultatsActivity::class.java).apply {
+                                putExtra("LOG_DATA", log)
+                            }
+                        )
                         finish()
                     }
                 )
